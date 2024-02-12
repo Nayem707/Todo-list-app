67a854d1489bd1b09e96cea2fc4eb44896be351a
nayem islam <gangsn490@gmail.com>
2024-02-12 14:25:41 +0600

    add header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 function App() {
   return (
     <>
-      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+      <header className='bg-green-800 h-[200px]'>
+        <div className='text-center py-10 space-y-2'>
+          <input type='text' />
+          <h1 className=' text-white text-3xl'>Task Manager</h1>
+
+          <div className=' text-center'>
+            <button className=' px-8 py-2 rounded-sm font-medium text-white bg-green-400'>
+              + Create
+            </button>
+          </div>
+        </div>
+      </header>
     </>
   );
 }
